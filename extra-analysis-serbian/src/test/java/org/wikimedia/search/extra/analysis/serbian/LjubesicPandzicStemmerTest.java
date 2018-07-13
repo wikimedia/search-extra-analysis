c5d49d7bab7be4c513ee4e8ce327da2bc7111660
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-07-13 10:38:38 +0200

    Introduce AssertJ to replace Hamcrest.
    AssertJ is the de facto successor of Hamcrest. It uses the type system
    to provide better IDE support than Hamcrest and tends to help write
    assertions that are more expressive.
    
    In this specific project, the changes are minor.
    
    See http://joel-costigliola.github.io/assertj/ for more details.
    
    Also upgraded parent-pom to latest version.
    
    Change-Id: I07efa6cb40c86c37491e6fcae19c23ca9e3fad5a

--- extra-analysis-serbian/src/test/java/org/wikimedia/search/extra/analysis/serbian/LjubesicPandzicStemmerTest.java
+++ extra-analysis-serbian/src/test/java/org/wikimedia/search/extra/analysis/serbian/LjubesicPandzicStemmerTest.java
@@ -1,61 +1,66 @@
 package org.wikimedia.search.extra.analysis.serbian;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class LjubesicPandzicStemmerTest extends TestCase {
+public class LjubesicPandzicStemmerTest {
 
 	LjubesicPandzicStemmer stemmer = new LjubesicPandzicStemmer();
 
-	public void testGeneralStemming() throws Exception {
+	@Test
+	public void testGeneralStemming() {
 		// General random Latin and Cyrillic words
-		assertEquals(stemmer.stemWord("abdominalni"), "abdominaln");
-		assertEquals(stemmer.stemWord("абдоминалне"), "abdominaln");
-		assertEquals(stemmer.stemWord("zabave"), "zabav");
-		assertEquals(stemmer.stemWord("забавама"), "zabav");
-		assertEquals(stemmer.stemWord("ћаф"), "ćaf");
-		assertEquals(stemmer.stemWord("жућкастосмеђим"), "žućkastosmeđ");
-		assertEquals(stemmer.stemWord("јужночешком"), "južnočešk");
-		assertEquals(stemmer.stemWord("ђуричковић"), "đuričković");
+		assertThat(stemmer.stemWord("abdominalni")).isEqualTo("abdominaln");
+		assertThat(stemmer.stemWord("абдоминалне")).isEqualTo("abdominaln");
+		assertThat(stemmer.stemWord("zabave")).isEqualTo("zabav");
+		assertThat(stemmer.stemWord("забавама")).isEqualTo("zabav");
+		assertThat(stemmer.stemWord("ћаф")).isEqualTo("ćaf");
+		assertThat(stemmer.stemWord("жућкастосмеђим")).isEqualTo("žućkastosmeđ");
+		assertThat(stemmer.stemWord("јужночешком")).isEqualTo("južnočešk");
+		assertThat(stemmer.stemWord("ђуричковић")).isEqualTo("đuričković");
 	}
 
-	public void testSuffixTransformations() throws Exception {
+	@Test
+	public void testSuffixTransformations() {
 		// words that use transformations
-		assertEquals(stemmer.stemWord("vašljivac"), "vašljivc");
-		assertEquals(stemmer.stemWord("bezvoljan"), "bezvoljn");
-		assertEquals(stemmer.stemWord("нормалан"), "normaln");
+		assertThat(stemmer.stemWord("vašljivac")).isEqualTo("vašljivc");
+		assertThat(stemmer.stemWord("bezvoljan")).isEqualTo("bezvoljn");
+		assertThat(stemmer.stemWord("нормалан")).isEqualTo("normaln");
 
 		// words that have transformation "suffixes" repeated earlier in the word
-		assertEquals(stemmer.stemWord("kovacevac"), "kovacevc"); // -vac
-		assertEquals(stemmer.stemWord("tractrac"), "tractrc"); // -rac
-		assertEquals(stemmer.stemWord("raveraverave"), "raveraverav"); // -rave
+		assertThat(stemmer.stemWord("kovacevac")).isEqualTo("kovacevc"); // -vac
+		assertThat(stemmer.stemWord("tractrac")).isEqualTo("tractrc"); // -rac
+		assertThat(stemmer.stemWord("raveraverave")).isEqualTo("raveraverav"); // -rave
 	}
 
-	public void testStopWords() throws Exception {
+	@Test
+	public void testStopWords() {
 		// "stop" words, should be unchanged
-		assertEquals(stemmer.stemWord("jesam"), "jesam");
-		assertEquals(stemmer.stemWord("moramo"), "moramo");
-		assertEquals(stemmer.stemWord("bijaše"), "bijaše");
-		assertEquals(stemmer.stemWord("želimo"), "želimo");
-		assertEquals(stemmer.stemWord("možeš"), "možeš");
+		assertThat(stemmer.stemWord("jesam")).isEqualTo("jesam");
+		assertThat(stemmer.stemWord("moramo")).isEqualTo("moramo");
+		assertThat(stemmer.stemWord("bijaše")).isEqualTo("bijaše");
+		assertThat(stemmer.stemWord("želimo")).isEqualTo("želimo");
+		assertThat(stemmer.stemWord("možeš")).isEqualTo("možeš");
 	}
 
-	public void testNonSerbianLatinAndCyrillic() throws Exception {
+	@Test
+	public void testNonSerbianLatinAndCyrillic() {
 		// Latin and Cyrillic words with non-Serbian characters
-		assertEquals(stemmer.stemWord("waxy"), "waxy"); // English
-		assertEquals(stemmer.stemWord("əliağa"), "əliağ"); // Azerbaijani
-		assertEquals(stemmer.stemWord("año"), "añ"); // Spanish
-		assertEquals(stemmer.stemWord("аблютомания"), "ablюtomaniя"); // Russian
-		assertEquals(stemmer.stemWord("вищій"), "viщій"); // Ukrainian (that second і is not an i!)
+		assertThat(stemmer.stemWord("waxy")).isEqualTo("waxy"); // English
+		assertThat(stemmer.stemWord("əliağa")).isEqualTo("əliağ"); // Azerbaijani
+		assertThat(stemmer.stemWord("año")).isEqualTo("añ"); // Spanish
+		assertThat(stemmer.stemWord("аблютомания")).isEqualTo("ablюtomaniя"); // Russian
+		assertThat(stemmer.stemWord("вищій")).isEqualTo("viщій"); // Ukrainian (that second і is not an i!)
 	}
 
-	public void testNonLatinNonCyrillic() throws Exception {
+	@Test
+	public void testNonLatinNonCyrillic() {
 		// words with characters other than Latin or Cyrillic
-		assertEquals(stemmer.stemWord("βικιπαίδεια"), "βικιπαίδεια"); // Greek
-		assertEquals(stemmer.stemWord("ვიკიპედია"), "ვიკიპედია"); // Georgian
-		assertEquals(stemmer.stemWord("위키백과"), "위키백과"); // Korean
-		assertEquals(stemmer.stemWord("ውክፔዲያ"), "ውክፔዲያ"); // Amharic
-		assertEquals(stemmer.stemWord("ᐅᐃᑭᐱᑎᐊ"), "ᐅᐃᑭᐱᑎᐊ"); // Inuktitut
+		assertThat(stemmer.stemWord("βικιπαίδεια")).isEqualTo("βικιπαίδεια"); // Greek
+		assertThat(stemmer.stemWord("ვიკიპედია")).isEqualTo("ვიკიპედია"); // Georgian
+		assertThat(stemmer.stemWord("위키백과")).isEqualTo("위키백과"); // Korean
+		assertThat(stemmer.stemWord("ውክፔዲያ")).isEqualTo("ውክፔዲያ"); // Amharic
+		assertThat(stemmer.stemWord("ᐅᐃᑭᐱᑎᐊ")).isEqualTo("ᐅᐃᑭᐱᑎᐊ"); // Inuktitut
 	}
 }
