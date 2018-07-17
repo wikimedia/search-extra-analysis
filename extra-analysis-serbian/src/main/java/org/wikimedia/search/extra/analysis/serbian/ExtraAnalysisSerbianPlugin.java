6f0c029773c734349fdb95aca1066e31a5720377
David Causse <dcausse@wikimedia.org>
2018-07-17 10:30:56 +0200

    Add elastic 6.3 support
    cleaned up source files:
    - spaces instead of tabs
    - LF instead of CRLF
    
    Bug: T198068
    Change-Id: Ia31b0ef62703837a632b44976de9f702c9e83900

--- extra-analysis-serbian/src/main/java/org/wikimedia/search/extra/analysis/serbian/ExtraAnalysisSerbianPlugin.java
+++ extra-analysis-serbian/src/main/java/org/wikimedia/search/extra/analysis/serbian/ExtraAnalysisSerbianPlugin.java
@@ -1,10 +1,9 @@
 package org.wikimedia.search.extra.analysis.serbian;
 
 import java.util.Collections;
-import java.util.Map;
+import java.util.List;
 
-import org.elasticsearch.index.analysis.TokenFilterFactory;
-import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
+import org.elasticsearch.index.analysis.PreConfiguredTokenFilter;
 import org.elasticsearch.plugins.AnalysisPlugin;
 import org.elasticsearch.plugins.Plugin;
 
@@ -13,12 +12,13 @@ import org.elasticsearch.plugins.Plugin;
  */
 public class ExtraAnalysisSerbianPlugin extends Plugin implements AnalysisPlugin {
 
-	/**
-	 * Register our stemmer.
-	 */
-	@Override
-	public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
-		return Collections.singletonMap("serbian_stemmer", SerbianStemmerTokenFilterFactory::new);
-	}
+    /**
+     * Register our stemmer.
+     */
+    @Override
+    public List<PreConfiguredTokenFilter> getPreConfiguredTokenFilters() {
+        return Collections.singletonList(PreConfiguredTokenFilter.singleton("serbian_stemmer",
+                true, SerbianStemmerTokenFilter::new));
+    }
 
 }
--- extra-analysis-serbian/src/main/java/org/wikimedia/search/extra/analysis/serbian/SerbianStemmerTokenFilter.java
+++ extra-analysis-serbian/src/main/java/org/wikimedia/search/extra/analysis/serbian/SerbianStemmerTokenFilter.java
@@ -11,29 +11,29 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 @SuppressFBWarnings(value = "EQ_DOESNT_OVERRIDE_EQUALS", justification = "Standard pattern for token filters.")
 public class SerbianStemmerTokenFilter extends TokenFilter {
 
-	private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
-	private static final LjubesicPandzicStemmer STEMMER = new LjubesicPandzicStemmer();
+    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    private static final LjubesicPandzicStemmer STEMMER = new LjubesicPandzicStemmer();
 
-	public SerbianStemmerTokenFilter(TokenStream in) {
-		super(in);
-	}
+    public SerbianStemmerTokenFilter(TokenStream in) {
+        super(in);
+    }
 
-	/* Marked final because "the TokenStream-API in Lucene is based on the
-	 * decorator pattern. Therefore all non-abstract subclasses must be final
-	 * or have at least a final implementation of incrementToken()! This is
-	 * checked when Java assertions are enabled."
-	 * https://lucene.apache.org/core/7_0_0/core/org/apache/lucene/analysis/TokenStream.html
-	 */
-	@Override
-	public final boolean incrementToken() throws IOException {
-		if (input.incrementToken()) {
-			String converted = STEMMER.stemWord(termAtt.toString());
-			if (converted != null) // if we can't stem it, return unchanged
-				termAtt.setEmpty().append(converted);
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /* Marked final because "the TokenStream-API in Lucene is based on the
+     * decorator pattern. Therefore all non-abstract subclasses must be final
+     * or have at least a final implementation of incrementToken()! This is
+     * checked when Java assertions are enabled."
+     * https://lucene.apache.org/core/7_0_0/core/org/apache/lucene/analysis/TokenStream.html
+     */
+    @Override
+    public final boolean incrementToken() throws IOException {
+        if (input.incrementToken()) {
+            String converted = STEMMER.stemWord(termAtt.toString());
+            if (converted != null) // if we can't stem it, return unchanged
+                termAtt.setEmpty().append(converted);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
